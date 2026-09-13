8aaeb53c6cb11f5896b75e5e58019fa2b46f77a2
Nimesh Dilshan <nimeshdilshan440@gmail.com>
2026-09-13 09:10:31 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="h-screen flex items-center justify-center bg-slate-50">
+    <h1 class="text-4xl font-semibold text-slate-800">Hello World</h1>
+</body>
+</html>
